6fdb462bae0d73b5246b6211c0f2e86a46669db7
Mehedi Hassan Durjoi <durjoi@asl.aero>
2021-03-22 12:00:37 +0600

    Mlat server for single target added

--- Decoder.py
+++ Decoder.py
@@ -75,7 +75,7 @@ class Decoder:
             # if len(msg) not in [4, 14, 28]:
             #     continue
             
-            if len(msg) not in [14]:
+            if len(msg) not in [28, 14, 4]:
                 continue
             
             self.messages_mlat.append([msg, ts])
--- TcpClient.py
+++ TcpClient.py
@@ -2,6 +2,7 @@ import socket
 import threading 
 from Decoder import Decoder
 import pyModeS as pms
+import pandas as pd
 
 ADSB_MESSAGES = {}
 DF_11 = {}
@@ -18,11 +19,15 @@ class TcpClient():
         self.socket = None
         self.buffersize = buffersize
         self.threadList = []
+        self.dataFrame = None
+        self.modes = None 
+        self.modeac = None
 
     def handle_connection(self, host, thread_id):
         addr = (host, self.port)
         
         self.socket.connect(addr)
+        self.dataFrame = pd.DataFrame()
         
         while True: 
             data = self.socket.recv(self.buffersize)
@@ -34,51 +39,86 @@ class TcpClient():
             messages_mlat = decoder.handle_messages()
             
             for msg in messages_mlat:
-                df = pms.df(msg[0])
+               
                 # ADS-B - Mode S Long 28 byte
-                if(df == 17):
-                    icao = pms.adsb.icao(msg[0])
-                    if msg[0] in ADSB_MESSAGES.keys():
-                        ADSB_MESSAGES[msg[0]][thread_id] = msg[1]
-                        print(f"{msg[0]} {ADSB_MESSAGES[msg[0]]}")
-                        # print(ALL_MESSAGES.keys())
-                    else: 
-                        ADSB_MESSAGES[msg[0]] = {}
-                        ADSB_MESSAGES[msg[0]][thread_id] = msg[1]
-                        ADSB_MESSAGES[msg[0]]["icao"] = icao
-                        print(f"{msg[0]} {ADSB_MESSAGES[msg[0]]}")
+                if len(msg[0]) == 4: 
+                    # icao = pms.adsb.icao(msg[0])
+                    
+                    incomming_dataFrame = pd.DataFrame([[msg[0], msg[1], thread_id, host]], columns=['Message', 'Timestamp', 'Receiver', 'Receiver IP'])
+                    if self.modeac is not None:
+                        # self.dataFrame = self.dataFrame.append(incomming_dataFrame)
+                        incomming_dataFrame.to_csv('modeac.csv', mode='a', header=None, index=False)  
+                    else:
+                        incomming_dataFrame.to_csv('modeac.csv', index=False)
+                        self.modeac = incomming_dataFrame
+                    incomming_dataFrame = None
+                    # if msg[0] in MODE_AC.keys():
+                    #     MODE_AC[msg[0]][thread_id] = msg[1]
+                    #     print(f"{msg[0]} {MODE_AC[msg[0]]}")
+                    #     # print(ALL_MESSAGES.keys())
+                    # else: 
+                    #     MODE_AC[msg[0]] = {}
+                    #     MODE_AC[msg[0]][thread_id] = msg[1]
+                    #     MODE_AC[msg[0]]["icao"] = icao
+                    #     print(f"{msg[0]} {MODE_AC[msg[0]]}")
+                else: 
+                    df = pms.df(msg[0])
+                    if df == 17:
                         
+                        icao = pms.adsb.icao(msg[0])
                         
-                # All Call Reply - Mode S short 14 byte
-                elif(df == 11):
-                    icao = pms.adsb.icao(msg[0])
-                    if icao in DF_11.keys():
-                        DF_11[icao]["msg"] = msg[0]
-                        DF_11[icao][str(thread_id)+"x"+str(DF_11[icao]["id"] + 1)] = msg[1]
-                        DF_11[icao]["id"] = DF_11[icao]["id"] + 1
-                        print(f"{icao} {DF_11[icao]}")
-                        # print(ALL_MESSAGES.keys())
-                    else: 
-                        DF_11[icao] = {}
-                        DF_11[icao]["id"] = 0
-                        DF_11[icao]["msg"] = msg[0]
-                        DF_11[icao][str(thread_id)+"x"+str(DF_11[icao]["id"] + 1)] = msg[1]
-                        DF_11[icao]["id"] = DF_11[icao]["id"] + 1
-                        DF_11[icao]["icao"] = icao
-                        print(f"{icao} {DF_11[icao]}")
+                        incomming_dataFrame = pd.DataFrame([[icao, msg[0], msg[1], thread_id, df, host]], columns=['Icao', 'Message', 'Timestamp', 'Receiver', 'df', 'Receiver IP'])
+                        if self.dataFrame is not None:
+                            # self.dataFrame = self.dataFrame.append(incomming_dataFrame)
+                            incomming_dataFrame.to_csv('adsb.csv', mode='a', header=None, index=False)  
+                        else:
+                            incomming_dataFrame.to_csv('adsb.csv', index=False)
+                            self.dataFrame = incomming_dataFrame
+                            # self.dataFrame.to_csv('adsb.csv', header=True, index=False) 
+                        incomming_dataFrame = None 
+                        # if msg[0] in ADSB_MESSAGES.keys():
+                        #     ADSB_MESSAGES[msg[0]][thread_id] = msg[1]
+                        #     print(f"{msg[0]} {ADSB_MESSAGES[msg[0]]}")
+                        #     # print(ALL_MESSAGES.keys())
+                        # else: 
+                        #     ADSB_MESSAGES[msg[0]] = {}
+                        #     ADSB_MESSAGES[msg[0]][thread_id] = msg[1]
+                        #     ADSB_MESSAGES[msg[0]]["icao"] = icao
+                        #     print(f"{msg[0]} {ADSB_MESSAGES[msg[0]]}")
+                        # print(self.dataFrame)
                         
+                            
+                    # All Call Reply - Mode S short 14 byte
+                    elif df == 11:
+                        icao = pms.adsb.icao(msg[0])
                         
-                # # Mode A/C - 4 byte
-                # else: 
-                #     if msg[0] in MODE_AC.keys():
-                #         MODE_AC[msg[0]][thread_id] = msg[1]
-                #         print(f"{msg[0]} {MODE_AC[msg[0]]}")
-                #         # print(ALL_MESSAGES.keys())
-                #     else: 
-                #         MODE_AC[msg[0]] = {}
-                #         MODE_AC[msg[0]][thread_id] = msg[1]
-                #         MODE_AC[msg[0]]["icao"] = icao
-                #         print(f"{msg[0]} {MODE_AC[msg[0]]}")
+                        incomming_dataFrame = pd.DataFrame([[icao, msg[0], msg[1], thread_id, df, host]], columns=['Icao', 'Message', 'Timestamp', 'Receiver', 'df', 'Receiver IP'])
+                        if self.modes is not None:
+                            # self.dataFrame = self.dataFrame.append(incomming_dataFrame)
+                            incomming_dataFrame.to_csv('modes.csv', mode='a', header=None, index=False)  
+                        else:
+                            incomming_dataFrame.to_csv('modes.csv', index=False)
+                            self.modes = incomming_dataFrame
+                        incomming_dataFrame = None
+                        # icao = pms.adsb.icao(msg[0])
+                        # if icao in DF_11.keys():
+                        #     DF_11[icao]["msg"] = msg[0]
+                        #     DF_11[icao][str(thread_id)+"x"+str(DF_11[icao]["id"] + 1)] = msg[1]
+                        #     DF_11[icao]["id"] = DF_11[icao]["id"] + 1
+                        #     print(f"{icao} {DF_11[icao]}")
+                        #     # print(ALL_MESSAGES.keys())
+                        # else: 
+                        #     DF_11[icao] = {}
+                        #     DF_11[icao]["id"] = 0
+                        #     DF_11[icao]["msg"] = msg[0]
+                        #     DF_11[icao][str(thread_id)+"x"+str(DF_11[icao]["id"] + 1)] = msg[1]
+                        #     DF_11[icao]["id"] = DF_11[icao]["id"] + 1
+                        #     DF_11[icao]["icao"] = icao
+                        #     print(f"{icao} {DF_11[icao]}")
+                            
+                            
+                    # # Mode A/C - 4 byte
+                    
             
     def handle_thread(self):
         
