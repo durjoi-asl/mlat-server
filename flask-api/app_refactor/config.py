8acbfd50beba7623d590877d2bb7c881575d5e75
Saadat <saadat@asl.aero>
2021-10-26 11:52:27 +0600

    update flask database auth config

--- flask-api/app_refactor/config.py
+++ flask-api/app_refactor/config.py
@@ -3,6 +3,8 @@ import os
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hello'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'postgresql://durjoi:mlat123@localhost/durjoi'
+        'postgresql://postgres:postgres@localhost/testThings'
+        # 'postgresql://durjoi:mlat123@localhost/durjoi' 
+        
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
